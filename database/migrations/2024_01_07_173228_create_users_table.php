eacc65f89ae7a28a66ef2ea68b05c2f5de7f054c
luluwaza <lucas.de.knibber@gmail.com>
2024-01-07 20:18:55 +0100

    minor changes

---
+++ database/migrations/2024_01_07_173228_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+// database/migrations/2022_01_01_create_users_table.php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->date('birthday')->nullable();
+            $table->string('avatar')->nullable();
+            $table->text('bio')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
+?> 
